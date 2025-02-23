d3d26bd75aed5b058fb796c73aaede7bb74669a1
Skanda <skanda.td@gmail.com>
2025-02-23 17:38:08 +0530

    Pushing Spring Boot Starters

---
+++ hello-app/src/main/java/com/hello/hello_app/HelloUsingGetRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.hello.hello_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloUsingGetRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloUsingGetRestApiApplication.class, args);
+	}
+
+}
---
+++ hello-app/src/test/java/com/hello/hello_app/HelloUsingGetRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.hello_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloUsingGetRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
